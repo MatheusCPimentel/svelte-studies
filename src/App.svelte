b9b8c65c41c8979081fd1e1d925f7613b44ca143
Matheus Pimentel <matheus08.pimentel@gmail.com>
2020-06-15 14:24:43 -0300

    Finish lesson 01

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script>
 	export let name;
+	let theColor = 'black';
+
+	const handleClick = () => {
+		theColor = 'orange';
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Testing!</p>
+	<p style="color: {theColor}">{theColor} color</p>
+	<button on:click={handleClick}>Click here to change to orange!</button>
+	<input type="text" bind:value={theColor}>
 </main>
 
 <style>
